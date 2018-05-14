8c39e5c24bc177eb37eb2828ac420075da11be55
ste92_unipd <stefano.tortora@phd.unipd.it>
2018-05-14 17:54:11 +0200

    adding event sender to cnbi loop

--- scripts/carnival.py
+++ scripts/carnival.py
@@ -4,7 +4,36 @@ import rospy
 import numpy
 import random
 from std_msgs.msg import *
+from cnbiros_bci.msg import TidMessage
 
+
+# Event class
+class Event:
+	begin = 1500
+	iccue = 600
+	icphase = 601
+	inccue = 602
+	incphase = 603
+	end = 1700
+
+	flagIC = False
+	flagINC1 = False
+	flagINC2 = False
+
+	duration = 0;
+
+	
+	def talker(self):
+		self.pub = rospy.Publisher("/rostid_ros2cnbi", TidMessage, queue_size=1000) #CNBIROS_BCI_TID_ROS2CNBI
+		self.rosidm = TidMessage()
+		self.rosidm.header.frame_id = "base_link" #CNBIROS_BCI_TID_FRAMEID
+		self.rosidm.version         = "0.0.1" #CNBIROS_BCI_TID_VERSION
+		self.rosidm.family 		   = 0 #IDMessage.FamilyBiosig
+		self.rosidm.description 	   = "ros_tidsender"
+		self.rosidm.event 		   = 666
+		self.rosidm.pipe			   = "/bus"
+
+	
 
 # Player class draws just a blue dot
 class Player:
@@ -94,15 +123,18 @@ class Bullet:
 
 # Game class
 class Game:
-	def __init__(self):
+	def __init__(self, event):
 		self.over = False
+		self.end = False
+		self.event = event
 		self.width = rospy.get_param('~screen_width', 640)
 		self.height = rospy.get_param('~screen_height', 480)
 		self.scale = rospy.get_param('~screen_scale', 3)
 		self.next_block_time = rospy.Time(0)
+		self.start_event_time = rospy.Time(0)
 
 		self.player = Player(self.width, self.height)
-		self.cluster_id_source = 0
+		self.cluster_id_source = 1
 		self.blocks = []
 		self.bullets = []
 
@@ -110,6 +142,9 @@ class Game:
 		self.block_interval_stddev = rospy.get_param('~block_interval_stddev', 1.0)
 		self.block_interval_min = rospy.get_param('~block_interval_min', 2.0)
 		self.block_interval_max = rospy.get_param('~block_interval_max', 5.0)
+		self.cluster_id_max = rospy.get_param('~trials_number', 15)
+
+		self.flag_new_cluster = True
 
 		self.input = False
 
@@ -133,7 +168,7 @@ class Game:
 		blocks = [Block((x, 30), self.cluster_id_source) for x in pos_x]
 		self.cluster_id_source += 1
 
-		return blocks
+		return 	blocks
 
 	def draw(self):
 		canvas = numpy.zeros((self.height, self.width, 3), numpy.uint8)
@@ -149,22 +184,96 @@ class Game:
 		canvas = cv2.resize(canvas, (canvas.shape[1] * self.scale, canvas.shape[0] * self.scale))
 		cv2.imshow('canvas', canvas)
 
+	def check_end(self):
+		if self.cluster_id_source > self.cluster_id_max:
+			self.end = True
+		if self.end and not len(self.blocks) > 0:
+			print("END")
+			################################
+			self.event.rosidm.header.stamp    = rospy.Time.now()
+			self.event.rosidm.event 		   = Event.end
+			self.event.pub.publish(self.event.rosidm)
+			################################
+
+			cv2.waitKey(2000)
+			self.over = True
+
 	def update(self):
 		# you can get block states like:
 		block_positions = [x.pos for x in self.blocks]
 		block_ids = [x.id for x in self.blocks]
 
-		# spawn new block
-		if rospy.Time.now() > self.next_block_time:
-			self.next_block_time = rospy.Time.now() + self.time_until_next_block()
+		# spawn new block cluster !!!! IC-CUE event !!!!
+		if rospy.Time.now() > self.next_block_time and not self.end and self.flag_new_cluster:
+			Event.duration = rospy.Time.now() - self.start_event_time
+			self.start_event_time = rospy.Time.now()
+			print(Event.duration.to_sec())
+			print("IC-CUE 600")
+			################################
+			self.event.rosidm.header.stamp    = rospy.Time.now()
+			self.event.rosidm.event 		   = Event.iccue
+			self.event.pub.publish(self.event.rosidm)
+			################################
+			Event.flagIC = False
+			#self.next_block_time = rospy.Time.now() + self.time_until_next_block()
 			cluster = self.generate_block_cluster(self.width, self.height)
 			self.blocks.extend(cluster)
+			self.flag_new_cluster = False
 
 		# update game elements
 		for block in self.blocks:
 			block.update()
 		# erase blocks who left the screen
-		self.blocks = [x for x in self.blocks if x.pos[0] > -100]
+		self.blocks = [x for x in self.blocks if x.pos[0] > -block.size[0]] # why not 0 ??? # -100
+		block_ids = [x.id for x in self.blocks]
+		
+		if len(self.blocks) > 0:
+			
+			block_width = rospy.get_param('~block_width', 40)
+
+			# !!!! IC-PHASE event !!!!
+			first_block_idx = block_ids.index(max(block_ids))
+			if self.blocks[first_block_idx].pos[0] - block_width / 2 < self.width / 2 and not Event.flagIC:
+				Event.duration = rospy.Time.now() - self.start_event_time
+				self.start_event_time = rospy.Time.now()
+				print(Event.duration.to_sec())
+				print("IC-PHASE 601")
+				################################
+				self.event.rosidm.header.stamp    = rospy.Time.now()
+				self.event.rosidm.event 		   = Event.icphase
+				self.event.pub.publish(self.event.rosidm)
+				################################
+				Event.flagIC = True
+				Event.flagINC1 = False
+		
+			# !!!! INC-CUE event !!!! 
+			if self.blocks[-1].pos[0] + block_width / 2 < self.width and not Event.flagINC1:
+				Event.duration = rospy.Time.now() - self.start_event_time
+				self.start_event_time = rospy.Time.now()
+				print(Event.duration.to_sec())
+				print("INC-CUE 602")
+				################################
+				self.event.rosidm.header.stamp    = rospy.Time.now()
+				self.event.rosidm.event 		   = Event.inccue
+				self.event.pub.publish(self.event.rosidm)
+				################################
+				Event.flagINC1 = True
+				Event.flagINC2 = False
+
+			# !!!! INC-PHASE event !!!!
+			if self.blocks[-1].pos[0] + block_width / 2 < self.width / 2 and not Event.flagINC2:
+				self.next_block_time = rospy.Time.now() + self.time_until_next_block()
+				Event.duration = rospy.Time.now() - self.start_event_time
+				self.start_event_time = rospy.Time.now()
+				print(Event.duration.to_sec())
+				print("INC-PHASE 603")
+				################################
+				self.event.rosidm.header.stamp    = rospy.Time.now()
+				self.event.rosidm.event 		   = Event.incphase
+				self.event.pub.publish(self.event.rosidm)
+				################################
+				Event.flagINC2 = True
+				self.flag_new_cluster = True
 
 		for bullet in self.bullets:
 			bullet.update()
@@ -189,17 +298,32 @@ class Game:
 
 		self.input = False
 
+		self.check_end()
+
 	# message callback to receive input signals
 	def callback(self, msg):
 		self.input = True
+		
 
 
 # entry point
 def main():
+	event = Event()
+	event.talker()
 	rospy.init_node('carnival')
 
-	game = Game()
+	game = Game(event)
 	sub = rospy.Subscriber('/shoot', Empty, game.callback)
+
+	game.draw()
+	game.start_event_time = rospy.Time.now()	
+	print("BEGIN")
+	################################
+	event.rosidm.header.stamp    = rospy.Time.now()
+	event.rosidm.event 		   = Event.begin
+	event.pub.publish(event.rosidm)
+	################################
+	cv2.waitKey(3000)
 
 	while not game.is_over() and not rospy.is_shutdown():
 		game.draw()
