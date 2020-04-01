e2f9b1e48bc946c5bf711c9dec945cb8d9d3c85d
GloriaBeraldo <gloria.beraldo@phd.unipd.it>
2020-04-01 19:29:27 +0200

    Minor change in the call of the reset service

--- src/rosneuro_feedback/smr_online.py
+++ src/rosneuro_feedback/smr_online.py
@@ -9,18 +9,19 @@ class SmrOnline(object):
 		self.event_pub = rospy.Publisher("/events/bus", NeuroEvent, queue_size=1000)
 
 		##### Configure subscriber #####
-		rospy.Subscriber("/integrated_neuroprediction", NeuroOutput, self.receive_probabilities)
+		rospy.Subscriber("/smrbci/integrated_neuroprediction", NeuroOutput, self.receive_probabilities)
 
 		##### Initialize probabilities #####
 		self.values = numpy.zeros(rospy.get_param('~n_classes'))
 
-	def receive_probabilities(msg):
+	def receive_probabilities(self, msg):
 		self.values = msg.softpredict.data
 
 	def reset_bci(self):
 		rospy.wait_for_service('/smrbci/reset')
+		resbci = rospy.ServiceProxy('/smrbci/reset', Empty)
     		try:
-			rospy.ServiceProxy('/smrbci/reset', Empty)
+			resbci()
 			return True
    		except rospy.ServiceException, e:
         		print "Service call failed: %s"
@@ -30,11 +31,12 @@ class SmrOnline(object):
 	def run(self):
 		
 		##### Configure protocol #####
-		sequence = config_trials(rospy.get_param('~n_classes'),rospy.get_param('~n_trials'))
+		n_classes = rospy.get_param('~n_classes')
+		sequence = config_trials(n_classes, rospy.get_param('~n_trials'))
 
 		##### Configure GUI engine #####
 		gui = SMRGUI(rospy.get_param('~window_height'),rospy.get_param('~window_width'),rospy.get_param('~window_scale'))
-		gui.init_bars(rospy.get_param('~n_classes'))
+		gui.init_bars(n_classes)
 		gui.draw()
 
 		print("[smr2class] Protocol starts")
@@ -57,15 +59,16 @@ class SmrOnline(object):
 			publish_neuro_event(self.event_pub, CLASS_EVENTS[idx]+OFF)
 
 			##### Continuous feedback #####
-			self.values = numpy.zeros(rospy.get_param('~n_classes'))
+			self.values = numpy.zeros(n_classes)
 			hit = False
-
 			self.reset_bci()
 			publish_neuro_event(self.event_pub, CFEEDBACK)
+
 			while not hit:
-				rospy.spin()
+				#rospy.spin()
+				
 				for c in range(n_classes):
-					value = normalize_probabilities(self.values[c], rospy.get_param('~threshold'), 1/rospy.get_param('~n_classes'))
+					value = normalize_probabilities(self.values[c], rospy.get_param('~threshold'), 1/n_classes)
 					gui.set_value_bars(value, c)
 					if value >= 1.0: 
 						hit = True
